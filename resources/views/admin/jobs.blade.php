0c4499d8bde25f32f04a6216736e7d2898458db4
sky lam <skyhilam@gmail.com>
2016-10-01 14:16:13 +0800

    update

---
+++ resources/views/admin/jobs.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin')
+
+
+@section('plugin')
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.4.0/github-markdown.min.css">
+
+<style>
+	.markdown-body {
+        box-sizing: border-box;
+        min-width: 200px;
+        max-width: 980px;
+        margin: 0 auto;
+        padding: 45px;
+    }
+</style>
+
+@endsection
+
+
+@section('content')
+
+<article class="markdown-body">
+
+@markdown
+# 用極簡單去思考和實現
+用極簡單的方法去做，先實現功能，使用，優化，創新！堅持一點一滴去做。  
+**不要心急賺錢，先規劃後實現！**
+
+## 項目開發
+1. message multiple send function
+2. message email style
+
+
+@endmarkdown
+
+    
+
+
+</article>
+
+
+@endsection
